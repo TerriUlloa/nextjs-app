6a34b54ae2298e74fd56ce7dff1cd3186a07a91c
Terri Ulloa <v-terriulloa@microsoft.com>
2023-04-13 12:45:36 -0700

    bundleAnalyzer config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+    enabled: process.env.ANALYZE === "true",
+});
 const nextConfig = {
     reactStrictMode: true,
     headers: async () => {
@@ -36,6 +39,4 @@ const nextConfig = {
      }
 }
 
-module.exports = {
-    output: "standalone", nextConfig,
-}
+module.exports = withBundleAnalyzer(nextConfig);
